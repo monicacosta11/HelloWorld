9dc051a4015044a4892d506aa23166e5998842e5
Monica Costa <monicacosta@usf.edu>
2019-01-15 17:11:48 -0500

    Version 3 HelloWorld

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Pause to diaplay Hello World!
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
